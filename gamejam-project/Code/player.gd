3bb915841b31c59b8274f3371098c415ab658e38
Niko <niko.p.virtanen@tuni.fi>
2026-05-06 13:09:26 +0300

    Platform riding
    Platform riding mekaniikka lisätty

--- gamejam-project/Code/player.gd
+++ gamejam-project/Code/player.gd
@@ -17,31 +17,30 @@ func _ready() -> void:
 	target_position = position
 
 func _physics_process(_delta: float) -> void:	
+	
+	ray.force_raycast_update()
 	if ray.is_colliding():
 		var collider = ray.get_collider()
-		if collider.is_in_group("platforms"):
+		if not is_moving and "is_rideable" in collider and collider.is_rideable:
 			current_platform = collider
 	else:
-		current_platform = null
+		if not is_moving:
+			current_platform = null
 		
 	if current_platform:
-		if "velocity" in current_platform and current_platform.velocity != Vector2.ZERO:
-			var drift = current_platform.velocity * _delta
-			global_position += drift
-			if is_moving:
-				target_position += drift
+		var drift = current_platform.velocity * _delta
+		global_position += drift
+		target_position += drift
 
 	if is_moving:
 		position = position.move_toward(target_position, tile_size * (1.0 / move_speed) * _delta)
 		if position == target_position:
 			is_moving = false
-			if sprite.is_playing() and sprite.animation == "jump":
-				sprite.play("idle")
 	else:
 		var input_direction = get_input_direction()
 		if input_direction != Vector2.ZERO:
 			move_to_tile(input_direction)
-
+			
 func get_input_direction() -> Vector2:
 	var dir = Vector2.ZERO
 	if Input.is_action_just_pressed("move_up"): dir = Vector2.UP
@@ -77,20 +76,15 @@ func move_to_tile(direction: Vector2) -> void:
 		sprite.flip_h = false
 		
 func _on_platform_detector_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Hazards"):
+		die()
 
-	if "velocity" in area:
-		current_platform = area
-		if not is_moving:
-			target_position.y = area.global_position.y
-
-func _on_platform_detector_area_exited(area: Area2D) -> void:
-	if current_platform == area:
-		current_platform = null
+func _on_platform_detector_area_exited(_area: Area2D) -> void:
+	pass
 
 func die() -> void:
 	is_moving = false
 	set_physics_process(false)
-	current_platform = null
 	
 	if sprite.sprite_frames.has_animation("die"):
 		sprite.play("die")
@@ -102,15 +96,3 @@ func die() -> void:
 
 	sprite.play("idle")
 	set_physics_process(true)
-
-func _on_boundary_body_entered(body: Node2D) -> void:
-	if body is Player:
-		body.die()
-
-func _on_alienship_body_entered(body: Node2D) -> void:
-	if body is Player:
-		body.die()
-
-func _on_asteroid_body_entered(body: Node2D) -> void:
-	if body is Player:
-		body.die()
