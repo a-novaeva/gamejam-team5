48182d664595c8ff891ff74309cb98f69d7e450e
Niko <niko.p.virtanen@tuni.fi>
2026-05-07 00:51:13 +0300

    Main menu
    Main menun start button liitetty intro sceneen

--- gamejam-project/Code/main_menu.gd
+++ gamejam-project/Code/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/intro_scene.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
