c6ea94772528d18ae0e776f905727c5d5d1c5938
Niko <niko.p.virtanen@tuni.fi>
2026-05-07 12:39:28 +0300

    you win screen added

---
+++ gamejam-project/Code/win_screen.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+func _on_play_again_pressed() -> void:
+	ScoreSystem.reset_game()
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
