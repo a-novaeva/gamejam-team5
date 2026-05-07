b788361175b3365d86887f412532a58eea2c2688
Niko <niko.p.virtanen@tuni.fi>
2026-05-07 22:39:54 +0300

    Settings menu + master volume slider
    Settings menu laitettu näkyviin ja lisätty slideri master volumelle

--- gamejam-project/Code/main_menu.gd
+++ gamejam-project/Code/main_menu.gd
@@ -7,32 +7,54 @@ extends Control
 func _ready():
 	main_menu_container.show()
 	difficulty_menu.hide()
+	$CanvasLayer/SettingsUI.hide()
 	ScoreSystem.hide()
+	
 	
 func _on_start_button_pressed():
 	main_menu_container.hide()
 	difficulty_menu.show()
 	
+	
 func _on_quit_button_pressed():
 	get_tree().quit()
+	
 
 func _on_settings_button_pressed() -> void:
-	pass # Replace with function body.
-
-
+	$CanvasLayer/MainMenuContainer.hide()
+	$CanvasLayer/SettingsUI.show()
+	
+	
+func _on_back_button_pressed() -> void:
+	$CanvasLayer/SettingsUI.hide()
+	$CanvasLayer/MainMenuContainer.show()
+	
+	
 func _on_easy_pressed() -> void:
 	ScoreSystem.show()
 	ScoreSystem.start_scoring()
+	$AudioStreamPlayer.stop()
 	GameManager.load_level("res://Scenes/level_1.tscn", 0)
 
 
 func _on_normal_pressed() -> void:
 	ScoreSystem.show()
 	ScoreSystem.start_scoring()
+	$AudioStreamPlayer.stop()
 	GameManager.load_level("res://Scenes/level_2.tscn", 1)
 
 
 func _on_hard_pressed() -> void:
 	ScoreSystem.show()
 	ScoreSystem.start_scoring()
-	pass # Replace with function body.
+	$AudioStreamPlayer.stop()
+	GameManager.load_level("res://Scenes/level_3.tscn", 2)
+
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	var bus_index = AudioServer.get_bus_index("Master")
+	var volume_db = linear_to_db(value)
+	
+	AudioServer.set_bus_volume_db(bus_index, volume_db)
+
+	AudioServer.set_bus_mute(bus_index, value < 0.01)
---
+++ gamejam-project/Code/volume_slider.gd
@@ -0,0 +1,12 @@
+extends HSlider
+
+@export var bus_name: String = "Master"
+var bus_index: int
+
+func _ready():
+	bus_index = AudioServer.get_bus_index(bus_name)
+	var volume_db = AudioServer.get_bus_volume_db(bus_index)
+	value = db_to_linear(volume_db)
+
+func _on_value_changed(new_value: float):
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(new_value))
