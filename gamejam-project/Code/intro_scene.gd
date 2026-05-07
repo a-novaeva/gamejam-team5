610dd3b4917fe7a6a99700c55f064dc5137bfad5
Niko <niko.p.virtanen@tuni.fi>
2026-05-07 11:37:07 +0300

    intro + main menu
    intro muutettu pelin alkuun jonkä jälkeen main menu ilmestyy

--- gamejam-project/Code/intro_scene.gd
+++ gamejam-project/Code/intro_scene.gd
@@ -14,10 +14,10 @@ func play_typing_effect():
 	intro_label.visible_ratio = 0.0
 	
 	var tween = create_tween()
-	tween.tween_property(intro_label, "visible_ratio", 1.0, 35.0)
-	await get_tree().create_timer(45.0).timeout
-	get_tree().change_scene_to_file("res://Scenes/difficulty_menu.tscn")
+	tween.tween_property(intro_label, "visible_ratio", 1.2, 25.0)
+	await get_tree().create_timer(30.0).timeout
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-		get_tree().change_scene_to_file("res://Scenes/difficulty_menu.tscn")
+		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
--- gamejam-project/Code/main_menu.gd
+++ gamejam-project/Code/main_menu.gd
@@ -1,10 +1,38 @@
 extends Control
 
+@onready var main_menu_container: VBoxContainer = $CanvasLayer/MainMenuContainer
+@onready var difficulty_menu = $CanvasLayer/DifficultyContainer
+
+
+func _ready():
+	main_menu_container.show()
+	difficulty_menu.hide()
+	ScoreSystem.hide()
+	
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/intro_scene.tscn")
+	main_menu_container.hide()
+	difficulty_menu.show()
 	
 func _on_quit_button_pressed():
 	get_tree().quit()
 
 func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_easy_pressed() -> void:
+	ScoreSystem.show()
+	ScoreSystem.start_scoring()
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+
+
+func _on_normal_pressed() -> void:
+	ScoreSystem.show()
+	ScoreSystem.start_scoring()
+	get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
+
+
+func _on_hard_pressed() -> void:
+	ScoreSystem.show()
+	ScoreSystem.start_scoring()
 	pass # Replace with function body.
